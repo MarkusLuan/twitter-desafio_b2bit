965f4e64e6a5e30c42ad56c96f157e5082ed59ff
markusluan <markus23_luan@hotmail.com>
2025-04-29 20:48:34 -0300

    Preparando pytest

---
+++ src/tests/test_api.py
@@ -0,0 +1,13 @@
+from app import app
+
+def test_not_found():
+    tester = app.test_client()
+    res = tester.get("/blabla")
+
+    assert res.status_code == 404
+    assert res.is_json == True
+    
+    j = res.json or {}
+
+    assert j["erro"] == True
+    assert j["texto"] == "Recurso não encontrado!"
