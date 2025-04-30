0414b187695c496b9cc8fb9f5cc9dd719f2f79d2
markusluan <markus23_luan@hotmail.com>
2025-04-30 18:18:29 -0300

    Criando exceptions

--- src/repositories/feed_repository.py
+++ src/repositories/feed_repository.py
@@ -2,6 +2,16 @@ from .abstract_repository import AbstractRepository
 
 from uuid import UUID
 
+from models import Feed
+from exceptions import feed_exceptions
+
 class FeedRepository (AbstractRepository):
     def get_by_uuid(self, _uuid: UUID):
-        return super().get_by_uuid(_uuid)
+        feed = Feed.query.filter(
+            Feed.uuid == _uuid
+        ).first()
+
+        if not feed:
+            raise feed_exceptions.FeedNotFoundException()
+
+        return feed
--- src/repositories/users_repository.py
+++ src/repositories/users_repository.py
@@ -4,10 +4,18 @@ from sqlalchemy import or_
 from uuid import UUID
 
 from models import User
+from exceptions import user_exceptions
 
 class UsersRepository (AbstractRepository):
     def get_by_uuid(self, _uuid: UUID):
-        return super().get_by_uuid(_uuid)
+        user = User.query.filter(
+            User.uuid == _uuid
+        ).first()
+
+        if not user:
+            raise user_exceptions.UserNotFoundException()
+
+        return user
     
     def fazer_login(self, usuario: str, senha: str):
         user = User.query.filter(or_(
@@ -16,7 +24,7 @@ class UsersRepository (AbstractRepository):
         )).first()
 
         if not user or user.senha != senha:
-            raise Exception ("Erro")
+            raise user_exceptions.InvalidLoginException()
         
         user.senha = None
         return user
