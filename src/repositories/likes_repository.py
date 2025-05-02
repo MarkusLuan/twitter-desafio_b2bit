de1834e812aee5ffc4af3e6ac12d0ec557dee0d5
markusluan <markus23_luan@hotmail.com>
2025-05-02 11:04:27 -0300

    Removendo like do post

--- src/repositories/likes_repository.py
+++ src/repositories/likes_repository.py
@@ -2,7 +2,7 @@ from .abstract_repository import AbstractRepository
 
 from uuid import UUID
 
-from models import Likes, Feed
+from models import Likes, Feed, User
 
 class LikesRepository (AbstractRepository):
     def get_by_uuid(self, _uuid: UUID):
@@ -22,6 +22,20 @@ class LikesRepository (AbstractRepository):
 
         return likes
     
+    def get_by_feed_uuid_and_user_uuid(self, _feed_uuid: UUID, _user_uuid: UUID):
+        like = Likes.query.join(
+            Feed,
+            Likes.feed_id == Feed.id
+        ).join(
+            User,
+            User.id == Likes.user_id
+        ).filter(
+            Feed.uuid == str(_feed_uuid),
+            User.uuid == str(_user_uuid)
+        ).first()
+
+        return like
+    
     def count_by_feed_uuid(self, _feed_uuid: UUID):
         likes = Likes.query.join(
             Feed,
@@ -36,4 +50,6 @@ class LikesRepository (AbstractRepository):
         ...
 
     def delete(self, _entity):
-        ...
+        db_session = self.db_session
+        db_session.delete(_entity)
+        db_session.commit()
--- src/resources/api/likes/routes.py
+++ src/resources/api/likes/routes.py
@@ -5,7 +5,6 @@ import uuid
 from models import Likes as LikesModel
 from repositories import FeedRepository, LikesRepository
 from ..abstract_routes import AbstractRoutes
-import app_singleton
 
 class Likes (AbstractRoutes):
     __repository = LikesRepository()
@@ -28,10 +27,7 @@ class Likes (AbstractRoutes):
             "feed_id": feed.id,
         }))
 
-        count = self.__repository.count_by_feed_uuid(uuid_feed)
-
-        
-        feed.count_likes = count
+        feed.count_likes = self.__repository.count_by_feed_uuid(uuid_feed)
         self.__feed_repository.update(feed)
 
         return jsonify(like.json)
@@ -39,4 +35,20 @@ class Likes (AbstractRoutes):
     def delete(self, uuid_feed: uuid.UUID):
         "Endpoint para descurtir uma postagem - Remoção fisica"
 
-        return jsonify({})
+        like = self.__repository.get_by_feed_uuid_and_user_uuid(
+            uuid_feed, 
+            self.logged_user_uuid
+        )
+
+        if not like:
+            return abort(404)
+        
+        deleted_like = like.json
+        self.__repository.delete(like)
+
+        # Atualiza cache de likes
+        feed = self.__feed_repository.get_by_uuid(uuid_feed)
+        feed.count_likes = self.__repository.count_by_feed_uuid(uuid_feed)
+        self.__feed_repository.update(feed)
+
+        return jsonify(deleted_like)
